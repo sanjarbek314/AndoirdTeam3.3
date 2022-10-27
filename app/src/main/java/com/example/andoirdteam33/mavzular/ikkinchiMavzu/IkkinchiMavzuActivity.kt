ed2cea4cb2324d32509bf77300678113c4f66acb
User1 <safarovsardor949@gmail.com>
2022-10-27 10:57:19 +0500

    2-mavzu

--- app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/IkkinchiMavzuActivity.kt
+++ app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/IkkinchiMavzuActivity.kt
@@ -1,12 +1,21 @@
 package com.example.androidteam32.mavzular.ikkinchiMavzu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.andoirdteam33.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.andoirdteam33.databinding.ActivityIkkinchiMavzuBinding
+import com.example.androidteam32.mavzular.ikkinchiMavzu.masala.BinchiMasala2Activity
 
 class IkkinchiMavzuActivity : AppCompatActivity() {
+    lateinit var binding: ActivityIkkinchiMavzuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ikkinchi_mavzu)
+        binding = ActivityIkkinchiMavzuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn1.setOnClickListener {
+            startActivity(Intent(this, BinchiMasala2Activity::class.java))
+        }
+
+
     }
 }
--- app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/BinchiMasala2Activity.kt
+++ app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/BinchiMasala2Activity.kt
@@ -3,9 +3,11 @@ package com.example.androidteam32.mavzular.ikkinchiMavzu.masala
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.andoirdteam33.R
+import com.example.andoirdteam33.databinding.ActivityBinchiMasala2Binding
 
 
-class BinchiMasala2 : AppCompatActivity() {
+class BinchiMasala2Activity : AppCompatActivity() {
+    lateinit var binding: ActivityBinchiMasala2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_binchi_masala2)
