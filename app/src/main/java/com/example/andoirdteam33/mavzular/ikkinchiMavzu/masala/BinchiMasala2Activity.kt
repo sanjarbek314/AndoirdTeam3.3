6640ee9216c5a187f609c74b403775bea3536b94
User <sanjarbekqahhorov314@gmail.com>
2022-10-28 19:27:33 +0500

    2-mavzu 1-2-masala

--- app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/BinchiMasala2Activity.kt
+++ app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/BinchiMasala2Activity.kt
@@ -2,6 +2,7 @@ package com.example.androidteam32.mavzular.ikkinchiMavzu.masala
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.andoirdteam33.R
 import com.example.andoirdteam33.databinding.ActivityBinchiMasala2Binding
 
@@ -10,38 +11,22 @@ class BinchiMasala2Activity : AppCompatActivity() {
     lateinit var binding: ActivityBinchiMasala2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityBinchiMasala2Binding.inflate(layoutInflater)
+        binding = ActivityBinchiMasala2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btn1.setOnClickListener {
-            var L=binding.L.text.toString().toInt()
-            if (L.toString().isNotEmpty()){
-              binding.misol.text="${L.toString().toInt()==1}"
+            var L = binding.L.text.toString().toInt()
+            if (L.toString().isNotEmpty()) {
+                if (100 == 1) {
+                    binding.misol.text = "${L.toString().toInt()}"
+                }
+
+
+            } else {
+                Toast.makeText(this, "raqam kiriting!!", Toast.LENGTH_SHORT).show()
             }
 
 
-
-
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
--- app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/IkkinchiMasala2.kt
+++ app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/IkkinchiMasala2.kt
@@ -2,11 +2,32 @@ package com.example.androidteam32.mavzular.ikkinchiMavzu.masala
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.andoirdteam33.R
+import com.example.andoirdteam33.databinding.ActivityIkkinchiMasala2Binding
+import com.example.andoirdteam33.databinding.ActivityYigirmanchiMasala1Binding
 
 class IkkinchiMasala2 : AppCompatActivity() {
+    lateinit var binding: ActivityIkkinchiMasala2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ikkinchi_masala2)
+        binding = ActivityIkkinchiMasala2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn1.setOnClickListener {
+            var M = binding.M.text.toString().toInt()
+            if (M.toString().isNotEmpty()) {
+                if (1 == 1000) {
+                    binding.misol.text = "${M.toString().toInt()}"
+                }
+
+
+            } else {
+                Toast.makeText(this, "raqam kiriting!!", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
     }
+
+
 }
