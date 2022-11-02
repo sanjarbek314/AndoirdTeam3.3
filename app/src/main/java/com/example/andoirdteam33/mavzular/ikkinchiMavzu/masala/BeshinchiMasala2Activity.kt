a375a1f8409628a866183d3efbbc29759b4ea5d8
User <sanjarbekqahhorov314@gmail.com>
2022-11-02 19:27:17 +0500

    2-mavzu 1-2-3-4-5masala

---
+++ app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/BeshinchiMasala2Activity.kt
@@ -0,0 +1,29 @@
+package com.example.andoirdteam33.mavzular.ikkinchiMavzu.masala
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.andoirdteam33.R
+import com.example.andoirdteam33.databinding.ActivityBeshinchiMasala2Binding
+
+class BeshinchiMasala2Activity : AppCompatActivity() {
+    lateinit var binding: ActivityBeshinchiMasala2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBeshinchiMasala2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn1.setOnClickListener {
+            var a = binding.A.text.toString().toInt()
+            var b = binding.B.text.toString().toInt()
+            if (a.toString().isNotEmpty() && b.toString().isNotEmpty()) {
+                if (a > 0 && b > 0) {
+                    if (a > b) {
+                        binding.misol.text = "${a / b}marta joyash tirish mumking"
+                    }
+                }
+            }
+
+        }
+
+
+    }
+}
