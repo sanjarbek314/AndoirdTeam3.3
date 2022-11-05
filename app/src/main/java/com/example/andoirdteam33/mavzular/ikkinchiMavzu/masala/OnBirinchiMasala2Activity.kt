d6c489ce3ed63c79dd6eabe14ba22886d98615c0
User1 <safarovsardor949@gmail.com>
2022-11-05 10:38:15 +0500

    2-mavzu-1-2-3-4-5-6-7-8-9-10-11-masala

---
+++ app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/OnBirinchiMasala2Activity.kt
@@ -0,0 +1,13 @@
+package com.example.andoirdteam33.mavzular.ikkinchiMavzu.masala
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.andoirdteam33.R
+
+class OnBirinchiMasala2Activity : AppCompatActivity() {
+    lateinit var b
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_birinchi_masala2)
+    }
+}
--- app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/OninchiMasala2Activity.kt
+++ app/src/main/java/com/example/andoirdteam33/mavzular/ikkinchiMavzu/masala/OninchiMasala2Activity.kt
@@ -3,19 +3,19 @@ package com.example.andoirdteam33.mavzular.ikkinchiMavzu.masala
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.andoirdteam33.databinding.ActivityYettinchiMasala2Binding
+import com.example.andoirdteam33.databinding.ActivityOninchiMasala2Binding
 
-class YettinchiMasala2Activity : AppCompatActivity() {
-    lateinit var binding: ActivityYettinchiMasala2Binding
+class OninchiMasala2Activity : AppCompatActivity() {
+    lateinit var binding: ActivityOninchiMasala2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityYettinchiMasala2Binding.inflate(layoutInflater)
+        binding = ActivityOninchiMasala2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btn1.setOnClickListener {
             var a = binding.A.text.toString()
-            if (a.isNotEmpty()) {
-                binding.misol.text = "raqamlar yig'indissi=${(a.toInt() / 10) + (a.toInt() % 10)}"
-            } else {
+            if (a.isNotEmpty() && a.length == 3){
+                binding.misol.text="birliklar xonasi=${a.toInt()%10}\no'nliklar xonasi=${a.toInt()/10}"
+            }else{
                 Toast.makeText(this, "raqam kiriting!!", Toast.LENGTH_SHORT).show()
             }
 
