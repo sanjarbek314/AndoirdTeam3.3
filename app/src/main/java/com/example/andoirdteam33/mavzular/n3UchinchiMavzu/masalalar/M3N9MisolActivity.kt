8eab396794c9074be71752380a075e9a138612fe
voin-546 <murodd546@gmail.com>
2023-04-25 09:39:45 +0500

    levle 123567

--- app/src/main/java/com/example/andoirdteam33/mavzular/n3UchinchiMavzu/masalalar/M3N9MisolActivity.kt
+++ app/src/main/java/com/example/andoirdteam33/mavzular/n3UchinchiMavzu/masalalar/M3N9MisolActivity.kt
@@ -5,10 +5,18 @@ import android.os.Bundle
 import com.example.andoirdteam33.databinding.ActivityM3N9MisolBinding
 
 class M3N9MisolActivity : AppCompatActivity() {
-    lateinit var binding:ActivityM3N9MisolBinding
-            override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var binding: ActivityM3N9MisolBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-                binding= ActivityM3N9MisolBinding.inflate(layoutInflater)
+        binding = ActivityM3N9MisolBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btn1.setOnClickListener {
+            var a = binding.A.text.toString()
+            var b = binding.B.text.toString()
+            if (a.isNotEmpty() && b.isNotEmpty()) {
+                binding.misol.text =
+                    "${a.toInt() % 2 == 1 && b.toInt() % 2 == 0}||${a.toInt() % 2 == 0 && b.toInt() % 2 == 1}||${a.toInt() % 2 == 0 && b.toInt() % 2 == 0}"
+            }
+        }
     }
 }
